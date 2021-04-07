babf3475691b54d271ffa2df65aded3517e0fcc2
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-04-07 11:17:21 -0300

    prevent NaN ids

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,14 @@ class Color {
         return {"r":r,"g":g,"b":b,"a":a}
     }
 }
+function addProcessor() {
+    const id = parseInt(prompt('Processor id'))
+    if(Number.isNaN(id)) return 
+    core.createProcessor('',parseInt(prompt('Processor id')))
+}
+function addDisplay() {
+    const id = parseInt(prompt('Screen id'))
+    const size = parseInt(prompt('Screen size'))
+    if(Number.isNaN(id) || Number.isNaN(size)) return
+    core.createDisplay(id, size)
+}
