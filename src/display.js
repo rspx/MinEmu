fab68e3486338ee18760830854e0d824271f410c
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-04-07 09:53:53 -0300

    fix error on display initialization

--- src/display.js
+++ src/display.js
@@ -3,7 +3,7 @@ class Display{
         this.displaysize = size==80?80:176
         this.id = id
         this.displayElement = displayElement
-        this.ctx = displayElement.children[2].getContext('2d')
+        this.ctx = displayElement.children[1].getContext('2d')
     }
     displayBuff = (buffer) =>{
       this.ctx.putImageData(new ImageData(buffer.buffer,buffer.size,buffer.size),0,0)
