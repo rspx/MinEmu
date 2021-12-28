1d9cb69700ad355ba60182b74d4fe20ba26f39e4
rspx <81689620+rspx@users.noreply.github.com>
2021-12-28 17:48:15 +0100

    Fixed line anti aliasing

--- src/display.js
+++ src/display.js
@@ -1,5 +1,5 @@
 class Display{
-    constructor(size,id){
+  constructor(size,id){
         this.displaysize = size==80?80:176
         this.id = id
         this.btn = this.createCanvas()
@@ -7,8 +7,8 @@ class Display{
         this.color = new Color(255,255,255,255)
         this.commands = []
         core.devices.push(this)
-    }
-    createCanvas = () =>{
+  }
+  createCanvas = () =>{
       const container = document.createElement("div")
       container.className = "display"
       const label = document.createElement("label")
@@ -33,46 +33,66 @@ class Display{
       })
       return container
   }
+  setPixel = (x,y) =>{
+    this.ctx.fillStyle = this.ctx.strokeStyle
+    this.ctx.fillRect( x, y, 1, 1 );
+    this.ctx.stroke()
+  }
+  plotLine = (x0, y0, x1, y1)=>
+  {
+     var dx =  Math.abs(x1-x0), sx = x0<x1 ? 1 : -1;
+     var dy = -Math.abs(y1-y0), sy = y0<y1 ? 1 : -1;
+     var err = dx+dy, e2;
+
+     for (;;){
+        this.setPixel(x0,y0);
+        if (x0 == x1 && y0 == y1) break;
+        e2 = 2*err;
+        if (e2 >= dy) { err += dy; x0 += sx; }
+        if (e2 <= dx) { err += dx; y0 += sy; }
+     }
+  }
   executeCommands = () =>{
-    for (let i=0;i<this.commands.length;i++){
-      let command = this.commands[i]
-      switch(command[0]){
-        case "color":
-          this.color = new Color(command[1],command[2],command[3],command[4])
-          break
-        case "clear":
-          let color = new Color(command[1],command[2],command[3],command[4])
-          this.ctx.beginPath()
-          this.ctx.strokeStyle = color.toRgba()
-          this.ctx.fillStyle = color.toRgba()
-          this.ctx.fillRect(0,0,this.displaysize,this.displaysize)
-          this.ctx.closePath()
-          break
-        case "rect":
-          this.ctx.beginPath();
-          this.ctx.strokeStyle = this.color.toRgba()
-          this.ctx.fillStyle = this.color.toRgba()
-          this.ctx.fillRect(command[1], command[2], command[3], command[4]);
-          this.ctx.closePath();
-          break
-        case "line":
-          this.ctx.beginPath()
-          this.ctx.strokeStyle = this.color.toRgba()
-          this.ctx.moveTo(command[1],command[2])
-          this.ctx.lineTo(command[3],command[4])
-          this.ctx.stroke()
-          this.ctx.closePath()
-          break
-        default:
-          logger.warn("Unknown command "+ command[0])
+      for (let i=0;i<this.commands.length;i++){
+        let command = this.commands[i]
+        switch(command[0]){
+          case "color":
+            this.color = new Color(command[1],command[2],command[3],command[4])
+            break
+          case "clear":
+            let color = new Color(command[1],command[2],command[3],command[4])
+            this.ctx.beginPath()
+            this.ctx.strokeStyle = color.toRgba()
+            this.ctx.fillStyle = color.toRgba()
+            this.ctx.fillRect(0,0,this.displaysize,this.displaysize)
+            this.ctx.closePath()
+            break
+          case "rect":
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = this.color.toRgba()
+            this.ctx.fillStyle = this.color.toRgba()
+            this.ctx.fillRect(command[1], command[2], command[3], command[4]);
+            this.ctx.closePath();
+            break
+          case "line":
+            this.ctx.beginPath()
+            this.ctx.strokeStyle = this.color.toRgba()
+            this.plotLine(command[1],command[2],command[3],command[4])
+            // this.ctx.moveTo(command[1],command[2])
+            // this.ctx.lineTo(command[3],command[4])
+            this.ctx.stroke()
+            this.ctx.closePath()
+            break
+          default:
+            logger.warn("Unknown command "+ command[0])
+        }
       }
-    }
-    this.commands = []
+      this.commands = []
   }
   displayBuff = (buffer) =>{
-    this.ctx.putImageData(new ImageData(buffer.buffer,buffer.size,buffer.size),0,0)
+      this.ctx.putImageData(new ImageData(buffer.buffer,buffer.size,buffer.size),0,0)
   }
   toString = () =>{
-    return this.size==80?"logic-display":"large-logic-display"
+      return this.size==80?"logic-display":"large-logic-display"
   }
 }
